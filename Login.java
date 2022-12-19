383cd241452318f43d19d9ff4fb42bf0d68766e0
aaishaltamimi <altamimiaisha141@gmail.com>
2022-12-19 21:09:14 +0300

    add login file

---
+++ Login.java
@@ -0,0 +1,29 @@
+package auth;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Login {
+
+	public static void main(String[] args) throws InterruptedException {
+		String email = "altamimiaisha141@gmail.com";
+		String pass = "12345";
+
+		System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.booking.com/");
+		driver.manage().window().maximize();
+		/*driver.findElement(By.id("email")).sendKeys(email);
+		Thread.sleep(3000);
+		driver.findElement(By.id("pass")).sendKeys(pass);
+		driver.findElement(By.name("login")).click();
+		driver.findElement(By.xpath("//div[@class='_9ay7']//a[contains(text(),'Forgotten password?')]")).click();
+		driver.findElement(By.xpath("//button[normalize-space()='Continue']")).click();*/
+		driver.findElement(By.id("ss")).sendKeys("amman");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+
+	}
+
+}
